4655341bb7588712e451524b6be49764982baebf
Tayyab <tayyab.hamza@outlook.com>
2024-10-22 22:44:46 +0500

    Dummy driver working

--- src/spi.c
+++ src/spi.c
@@ -7,14 +7,43 @@
 #include <linux/io.h>
 #include <linux/log2.h>
 
+// Definations
 #define NO_ERROR        0
 #define ERROR           1
 
-MODULE_LICENSE("GPL");
+// Structures
 
 struct spi_device_state {
     void __iomem *base_address;
 };
+
+static struct platform_driver spi_driver = {
+	.probe = spi_probe,
+	.remove = spi_remove,
+	.driver = {
+		.name = "Salman-Tayyab-Zawaher's_driver",
+		.of_match_table = matching_devices
+	},
+};
+
+static const struct of_device_id matching_devices[] = {
+	{ .compatible = "sifive,spi0", },
+	{}
+};
+
+// Functions
+
+static int __init spi_init(void)
+{
+	printk("Hello, world!\n");
+  	return NO_ERROR;
+}
+
+static void __exit spi_exit(void)
+{
+	printk(KERN_INFO "Goodbye, world!\n");
+}
+
 
 static int spi_probe(struct platform_device *dev)
 {
@@ -33,17 +62,8 @@ static int spi_remove(struct platform_device *dev)
 	return NO_ERROR;
 }
 
-static const struct of_device_id matching_devices[] = {
-	{ .compatible = "sifive,spi0", },
-	{}
-};
+// Register functions / structures with kernel
 MODULE_DEVICE_TABLE(of, matching_devices);
+module_init(spi_init);
+module_exit(spi_exit);
 
-static struct platform_driver spi_driver = {
-	.probe = spi_probe,
-	.remove = spi_remove,
-	.driver = {
-		.name = "Salman-Tayyab-Zawaher's_driver",
-		.of_match_table = matching_devices
-	},
-};
